71a678ac9fc19ddfd83370238561de5651135c74
Jennp1987 <44036108+Jennp1987@users.noreply.github.com>
2021-10-11 17:32:18 -0700

    updated script

--- js/script.js
+++ js/script.js
@@ -98,16 +98,16 @@ function updateDisplay() {
         orderAndBillingSpan.style.display = "none";
         orderDetails.style.display = "block";
         orderDetails.style.border = "1px black solid";
-        // if (phase === phases.crustToppings) {
-        //     doughSpan.classList.toggle("phaseCrustTypeInToppings");
+        if (phase === phases.crustToppings) {
+            doughSpan.classList.toggle("phaseCrustTypeInToppings");
            
             
-        // } else if (phase !== phases.crustToppings) {
-        //     doughSpan.style.marginLeft = "0em"; 
-        //     doughSpan.classList.remove("phaseCrustTypeInToppings");
+        } else if (phase !== phases.crustToppings) {
+            doughSpan.style.marginLeft = "0em"; 
+            doughSpan.classList.remove("phaseCrustTypeInToppings");
            
             
-        // }
+        }
 
 
 
